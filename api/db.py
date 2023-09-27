daaf9544c81b6973f3779c01456ae0f6f655ab99
TheAlpha16 <101546002+TheAlpha16@users.noreply.github.com>
2023-09-27 14:25:58 +0530

    added docker compose file

--- api/db.py
+++ api/db.py
@@ -3,17 +3,16 @@ import psycopg2
 config = {
     "user": "postgres",
     "password": "idkthepassword",
-    "host": "0.0.0.0",
-    "database": "newsDB",
+    "host": "0.0.0.0"
 }
-conn = psycopg2.connect(**config)
+conn = psycopg2.connect(host=config["host"], user=config["user"], password=config["password"])
 
 
 def getarticles(page, limit):
     offset = int(page) * int(limit)
     curr = conn.cursor()
     try:
-        curr.execute("""SELECT id, link, title, content, created, sentiment, department FROM articles LIMIT %d OFFSET %d;""", (int(limit), int(offset)))
+        curr.execute("""SELECT id, link, title, content, created, sentiment, department FROM articles LIMIT %s OFFSET %s;""", (int(limit), int(offset)))
     except psycopg2.ProgrammingError:
         return []
     data = curr.fetchall()
