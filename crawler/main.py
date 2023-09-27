4237ba1327c8cd6b9c98b1cc871f8b56a57da22d
Atish03 <atishpersonal003@gmail.com>
2023-09-27 20:22:20 +0530

    integrated db and scrapper

--- crawler/main.py
+++ crawler/main.py
@@ -7,36 +7,36 @@ import json
 
 import random
 
-mydb = psycopg2.connect(
-    host = "localhost",
-    user = "postgres",
-    password = "idkthepassword",
-    database = "newsDB"
-)
-
 class App:
     def __init__(self):
-        self.cursor = mydb.cursor()
+        self.mydb = psycopg2.connect(
+            host = "localhost",
+            user = "postgres",
+            password = "idkthepassword",
+        )
+        self.cursor = self.mydb.cursor()
         connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
         self.channel = connection.channel()
         self.channel.queue_declare(queue = "toCrawl")
 
     def insertArticle(self, article):
-        self.cursor.execute(f'INSERT INTO articles (link, title, content, created, sentiment, department, language) VALUES ({article["url"]}, {article["headline"]}, {article["articleBody"]}, {datetime.datetime.now().isoformat()}, {article["sentiment"]}, {article["department"]}, {article["inLanguage"]})')
+        self.cursor.execute("INSERT INTO articles (link, title, content, created, sentiment, department, lang) VALUES (%s, %s, %s, %s, %s, %s, %s)", ( article["url"], article["headline"], article["articleBody"], datetime.datetime.now().isoformat(), article["sentiment"], article["department"], article["inLanguage"] ))
 
     def callback(self, ch, method, properties, body):
         req = json.loads(body)
         crawler = Crawl(lang = req['lang'], provider = req['provider'])
-        print(req)
         
         for url in req["urls"]:
             article = crawler.getNews(url)
+            
+            if article:
+                # Do sentiment analysis + department prediction
+                article["sentiment"] = round(random.random(), 3)
+                article["department"] = "noone"
 
-            # Do sentiment analysis + department prediction
-            article["sentiment"] = round(random.random(), 3)
-            article["department"] = "noone"
+                self.insertArticle(article)
 
-            self.insertArticle(article)
+        self.mydb.commit()
 
     def listen(self):
         self.channel.basic_consume(queue = "toCrawl", on_message_callback = self.callback, auto_ack = True)
