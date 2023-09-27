ed7963907e3f5219421238fc6a4196b45e7a6596
TheAlpha16 <101546002+TheAlpha16@users.noreply.github.com>
2023-09-27 13:41:08 +0530

    added db config

--- api/app.py
+++ api/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template, request
+from db import getarticles
 
 app = Flask(__name__)
 routes = ["/ping"]
@@ -15,7 +16,20 @@ def ping():
 @app.route("/articles", methods=["POST"])
 def getArticles():
     page = request.args.get("page")
-    
+    limit = request.args.get("limit")
+    if page == None:
+        page = "0"
+    if limit == None:
+        limit = "100"
+    if not page.isnumeric():
+        return {"status": "fail", "data": "invalid request - page"}
+    if not limit.isnumeric():
+        return {"status": "fail", "data": "invalid request - limit"}
+    data = getarticles(page, limit)
+    if data == []:
+        return {"status": "fail", "data": "DBError"}
+    return {"status": "success", "data": data}
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=9999)
--- api/db.py
+++ api/db.py
@@ -1,12 +1,22 @@
-import mysql.connector
+import psycopg2
 
 config = {
-    "user": "admin",
+    "user": "postgres",
     "password": "idkthepassword",
-    "host": "127.0.0.1",
+    "host": "0.0.0.0",
+    "database": "newsDB",
 }
-conn = mysql.connector.connect(**config)
+conn = psycopg2.connect(**config)
 
 
-def getarticles(page):
-    query = """SELECT """
+def getarticles(page, limit):
+    offset = int(page) * int(limit)
+    curr = conn.cursor()
+    try:
+        curr.execute("""SELECT id, link, title, content, created, sentiment, department FROM articles LIMIT %d OFFSET %d;""", (int(limit), int(offset)))
+    except psycopg2.ProgrammingError:
+        return []
+    data = curr.fetchall()
+    curr.close()
+
+    return data
